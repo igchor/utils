46724c2c2ac8e1936c69dcbc6b98f13507eba6e5
Igor Chorążewicz <igor.chorazewicz@intel.com>
2019-05-21 16:43:24 +0200

    Create parse_compiler_errors.sh

---
+++ parse_compiler_errors.sh
@@ -0,0 +1,1 @@
+awk -F ":" '{ filename=$1 ; linenumber=$2 ;  charnumber=$3 ; type=$4; if (type == " error") { system("sed -i \""linenumber"s/\\(.\\{"(charnumber-1)"\\}\\)\\(.*\\)/\\1std::\\2/\"g "filename"");  } }' xxx.txt
