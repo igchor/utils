62d9c5f4274638df9d59ddb846b82d63aa46a792
Igor Chorążewicz <igor.chorazewicz@intel.com>
2019-05-21 17:11:05 +0200

    Update parse_compiler_errors.sh

--- parse_compiler_errors.sh
+++ parse_compiler_errors.sh
@@ -1,1 +1,8 @@
-awk -F ":" '{ filename=$1 ; linenumber=$2 ;  charnumber=$3 ; type=$4; if (type == " error") { system("sed -i \""linenumber"s/\\(.\\{"(charnumber-1)"\\}\\)\\(.*\\)/\\1std::\\2/\"g "filename"");  } }' xxx.txt
+# run.sh
+make 2> compiler_out.txt && exit 1
+awk -F ":" '{ filename=$1 ; linenumber=$2 ;  charnumber=$3 ; type=$4; if (type == " error") { system("sed -i \""linenumber"s/\\(.\\{"(charnumber-1)"\\}\\)\\(.*\\)/\\1std::\\2/\"g "filename""); exit 0; } }' compiler_out.txt
+########################################################
+
+
+# ./run.sh
+# while [ $? -eq 0 ]; do ./run.sh ; done
